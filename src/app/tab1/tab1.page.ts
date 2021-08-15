f019f9562dbbfd06354360de6fe47afe6f886f86
leovasc5 <leovasc5@hotmail.com>
2021-08-15 10:08:00 -0300

    PAM2 Aula 18, inclusão do componente alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
 
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Confirmação',
+      message: 'VocÇe deseja adicionar esse filme aos <strong>favoritos</strong>?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Adicionar',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos.',
+      duration: 3000,
+      color: 'success'
+    });
+    toast.present();
+  }
 }
